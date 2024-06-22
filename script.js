d310235aa86f0d67d1991a0a733dd8c146e48376
martin dubuisson <martin.dbssn@gmail.com>
2024-06-22 15:28:16 +0200

    Modify readme and create the calculator

--- script.js
+++ script.js
@@ -4,21 +4,47 @@ const year = document.getElementById('year')
 const dayText = document.getElementById("daysText")
 const monthText = document.getElementById("monthsText")
 const yearText = document.getElementById('yearsText')
-const button = document.getElementById("button")
+const form = document.querySelector("form")
 const dayError = document.getElementById('day-error')
 const monthError = document.getElementById('month-error')
 const yearError = document.getElementById('year-error')
 
-button.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault(); 
-  dayText.innerText = `${day.value}`;
-  monthText.innerText = `${month.value}`;
-  yearText.innerText = `${year.value}`;
+
+  const dayd = parseInt(day.value);
+  const monthd = parseInt(month.value);
+  const yeard = parseInt(year.value);
+ 
+  day.required = true;
+  month.required = true;
+  year.required = true;
+
+  const birthday = new Date(yeard, monthd - 1, dayd)
+  const today = new Date();
+
+  let ageInDays = today.getDate() - birthday.getDate();
+  let ageInMonths = today.getMonth() - birthday.getMonth();
+  let ageInYears = today.getFullYear() - birthday.getFullYear();
+
+  if (ageInDays < 0) {
+    ageInMonths--;
+    const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 0);
+    ageInDays += lastMonth.getDate();
+  }
+  if (ageInMonths < 0) {
+    ageInYears--;
+    ageInMonths += 12;
+  }
+
+  dayText.innerText = ageInDays;
+  monthText.innerText = ageInMonths;
+  yearText.innerText = ageInYears;
+
+  
 });
 
-day.required = true;
-month.required = true;
-year.required = true;
+
 
 
 
